5221be57c18ee15ce2e273b0ef839d8f0a8b591d
Peter Johnson <peterandersjohnson@outlook.com>
2024-12-23 15:58:12 -0600

    Additional pathing tweaks...

--- Build.sh
+++ Build.sh
@@ -1,15 +1,17 @@
 #!/bin/ash
+echo Starting build script...
 SCRIPTPATH=$(dirname "$SCRIPT")
 echo "$SCRIPTPATH"
 echo Copying root assets so github readme file can be embedded into website file.
 cp -R -a $SCRIPTPATH/images/. $SCRIPTPATH/AuroLeap/modules/ROOT/images/
-cp $SCRIPTPATH/README.adoc $SCRIPTPATH/AuroLeap/modules/ROOT/pages/README.adoc
+cp $SCRIPTPATH/readme.adoc $SCRIPTPATH/AuroLeap/modules/ROOT/pages/readme.adoc
+ls $SCRIPTPATH/AuroLeap/modules/ROOT/pages/
 npm i antora
 npm i @antora/lunr-extension
 npm i asciidoctor-kroki
 npx antora antora-playbook.yml
-rm ./AuroLeap/modules/ROOT/pages/README.adoc
-for file in ./images/*
+rm $SCRIPTPATH/AuroLeap/modules/ROOT/pages/readme.adoc
+for file in $SCRIPTPATH/images/*
 do
 	name=`basename $file`
 	rmpath=$SCRIPTPATH/AuroLeap/modules/ROOT/images/$name
