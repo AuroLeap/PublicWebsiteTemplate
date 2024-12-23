f5b3b794abf3a575512af8ed90907eef3d7f565d
Peter Johnson <peterandersjohnson@outlook.com>
2024-12-22 20:59:27 -0600

    Fix to remove duplicate images after being copying to the images folder.

--- CloudFlareBuild.sh
+++ CloudFlareBuild.sh
@@ -1,6 +1,5 @@
 #!/bin/ash
-#cp -R -a ./rootimages ./AuroLeap/modules/ROOT/
-#cp -R -a ./rootimages ./AuroLeap/modules/ROOT/pages/
+echo Copying root assets so github readme file can be embedded into website file.
 cp -R -a ./images/. ./AuroLeap/modules/ROOT/images/
 cp ./README.adoc ./AuroLeap/modules/ROOT/pages/README.adoc
 npm i antora
@@ -8,5 +7,10 @@ npm i @antora/lunr-extension
 npm i asciidoctor-kroki
 npx antora antora-playbook.yml
 rm ./AuroLeap/modules/ROOT/pages/README.adoc
-#rm ./AuroLeap/modules/ROOT/pages/README.adoc
-#cd new-files ; find . -exec rm -rf path/to/other-directory/{} \;
+for file in ./images/*
+do
+	name=`basename $file`
+	rmpath=./AuroLeap/modules/ROOT/images/$name
+	echo Removing copied asset "$rmpath"
+	rm -r "$rmpath"
+done
