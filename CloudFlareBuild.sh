2936012e06fab6e7bb363fb593b4e26bb06a9b49
Peter Johnson <peterandersjohnson@outlook.com>
2024-12-22 19:22:40 -0600

    Minor cleanup, need to delete duplicate images after build potentially, or find another way to ignore.

--- CloudFlareBuild.sh
+++ CloudFlareBuild.sh
@@ -1,7 +1,12 @@
 #!/bin/ash
-cp -R -a ./images/. ./AuroLeap/modules/ROOT/images
+#cp -R -a ./rootimages ./AuroLeap/modules/ROOT/
+#cp -R -a ./rootimages ./AuroLeap/modules/ROOT/pages/
+cp -R -a ./images/. ./AuroLeap/modules/ROOT/images/
 cp ./README.adoc ./AuroLeap/modules/ROOT/pages/README.adoc
 npm i antora
 npm i @antora/lunr-extension
 npm i asciidoctor-kroki
 npx antora antora-playbook.yml
+rm ./AuroLeap/modules/ROOT/pages/README.adoc
+#rm ./AuroLeap/modules/ROOT/pages/README.adoc
+#cd new-files ; find . -exec rm -rf path/to/other-directory/{} \;
--- StartCmd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/ash
-npm i @antora/lunr-extension
-npm i asciidoctor-kroki
-antora antora-playbook.yml
