0eca4b056ea7639ad83fe361c8b39eea75a2f07b
Peter Johnson <peterandersjohnson@outlook.com>
2024-12-23 10:51:57 -0600

    Content update + Build script fixes
    - Update readme for more content
    - Fix podman call / dockerfile entrypoint file
    - Clean up powershell script code

--- ContainerSupport/RunContainer.ps1
+++ ContainerSupport/RunContainer.ps1
@@ -1,14 +1,13 @@
 "Running container in path:"
 $setdir = (Split-Path $PSScriptRoot) + "\"
 $setdir
-$runscript = $setdir + "CloudFlareBuild.sh"
+$runscript = $setdir + "Build.sh"
 $error.clear()
+podman machine start
 try { 
-#podman run -v ${setdir}:/antora:Z --rm -t local/antora:auroleap antora.yml
-#podman run -v ${setdir}:/antora:Z --rm -t local/antora:auroleap
+
 podman run -v ${setdir}:/antora:Z --rm -t local/websitedocker:main
-#podman run -v ${setdir}:/antora:Z --entrypoint="./antora/CloudFlareBuild.sh" --rm -t cloudflare/cloudflared:latest
-#podman run -v ${setdir}:/antora:Z --entrypoint="/antora/CloudFlareBuild.sh" --rm -t cloudflare/cloudflared:latest
+
 }
 catch { "Error occured" }
 Write-Host 'Press any key to close...';
